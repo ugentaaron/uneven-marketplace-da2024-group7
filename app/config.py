442d75ffce94017b6522195243a12d2724c0a812
Arthur <arthur.fryns@gmail.com>
2024-11-08 13:28:34 +0100

    2
    Supabase added

--- app/config.py
+++ app/config.py
@@ -1,4 +1,4 @@
 class Config: 
-    SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.username:password@localhost:port/dbname'
+    SECRET_KEY = 'FEZ_agb.mcb4rvb.xyz'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres.ecbvpmnwlkgavmfxwhpf:[YOUR-PASSWORD]@aws-0-eu-west-3.pooler.supabase.com:6543/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
